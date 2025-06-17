27464fbe8a374307ae0a4292cdc642f4f01dab14
ottavianorob <me@robertoottaviano.it>
2025-06-17 15:47:21 +0200

    last

--- milano-historic-places/src/App.tsx
+++ milano-historic-places/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import MapView from './components/MapView';
 import BottomSheet from './components/ui/BottomSheet';
+import BottomNav from './components/ui/BottomNav';
 import type { Place } from './components/types';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       {selected && (
         <BottomSheet place={selected} onClose={() => setSelected(null)} />
       )}
+      <BottomNav />
     </>
   );
 }
