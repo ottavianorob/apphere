39dbd3a5d0ebc3e539443bc2c11db047fee20946
ottavianorob <me@robertoottaviano.it>
2025-06-17 05:01:31 +0200

    Update types.ts

--- milano-historic-places/src/components/types.ts
+++ milano-historic-places/src/components/types.ts
@@ -1,7 +1,14 @@
+// src/components/types.ts
 export type Place = {
   id: string;
   title: string;
   geometry: { type: 'Point'; coordinates: [number, number] };
   image: string;
   teaser: string;
+};
+
+export type Itinerary = {
+  id: string;
+  title: string;
+  stops: string[];
 };
