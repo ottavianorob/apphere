24df2de2fddf5c2b0b0dae4cfdaf152ec5c59804
ottavianorob <me@robertoottaviano.it>
2025-06-17 14:33:27 +0200

    Update postcss.config.cjs

--- milano-historic-places/postcss.config.cjs
+++ milano-historic-places/postcss.config.cjs
@@ -1,8 +1,7 @@
+// postcss.config.cjs
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')({
-      config: './tailwind.config.cjs', // percorso del file Tailwind
-    }),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
